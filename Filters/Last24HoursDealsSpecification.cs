7ed0bbac446628aec8e2f1e0ad53b7ff6a962cfc
Ilya Safonov <illia220011@gmail.com>
2023-04-14 00:54:03 +0300

    Filters has been created.

---
+++ Filters/Last24HoursDealsSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+using Filters.Abstractions;
+using System.Linq.Expressions;
+
+namespace Filters
+{
+    public class Last24HoursDealsSpecification : AbstractFilterSpecification<Deal>
+    {
+        public override Expression<Func<Deal, bool>> SpecificationExpression =>
+            (deal) => deal.Status == Entities.Enums.DealStatus.Success && deal.UpdatedStatusTime.AddHours(24).ToUniversalTime() >= DateTime.Now.ToUniversalTime();
+    }
+}
