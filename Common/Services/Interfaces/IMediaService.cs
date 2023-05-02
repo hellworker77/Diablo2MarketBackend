2c99d7bab2297d86587fe59a0603a118307b3dc8
Ilya Safonov <illia220011@gmail.com>
2023-05-03 02:51:44 +0300

    Media service has been added.

---
+++ Common/Services/Interfaces/IMediaService.cs
@@ -0,0 +1,26 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Services.Interfaces
+{
+    public interface IMediaService
+    {
+        Task<MediaDto> GetByIdAsync(Guid mediaId,
+            CancellationToken cancellationToken);
+        Task CreateToItemAsync(MediaDto mediaDto,
+            Guid itemId,
+            CancellationToken cancellationToken);
+        Task CreateToUserAsync(MediaDto mediaDto,
+            Guid userId,
+            CancellationToken cancellationToken);
+        Task UpdateAsync(MediaDto mediaDto,
+            Guid mediaId,
+            CancellationToken cancellationToken);
+        Task DeleteAsync(Guid mediaId,
+            CancellationToken cancellationToken);
+    }
+}
---
+++ Common/Services/MediaService.cs
@@ -0,0 +1,83 @@
+﻿using Common.Exceptions;
+using Common.Mappers;
+using Common.Models;
+using Common.Services.Interfaces;
+using Dal.Data;
+using Dal.Interfaces;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Services
+{
+    public class MediaService : IMediaService
+    {
+        private readonly IMediaRepository _mediaRepository;
+        private readonly MediaMapper _mapper;
+
+        public MediaService(IMediaRepository mediaRepository,
+            MediaMapper mapper)
+        {
+            _mediaRepository = mediaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<MediaDto> GetByIdAsync(Guid mediaId, 
+            CancellationToken cancellationToken)
+        {
+            var media = await _mediaRepository.GetByIdAsync(mediaId, cancellationToken);
+            if (media is null)
+            {
+                throw new EntityNotFoundException(typeof(Media), mediaId);
+            }
+
+            return _mapper.Map(media);
+        }
+
+        public async Task CreateToItemAsync(MediaDto mediaDto, 
+            Guid itemId,
+            CancellationToken cancellationToken)
+        {
+            var media = _mapper.ReverseMap(mediaDto);
+            media.ItemId = itemId;
+
+            await _mediaRepository.CreateAsync(media, cancellationToken);
+        }
+
+        public async Task CreateToUserAsync(MediaDto mediaDto, 
+            Guid userId, 
+            CancellationToken cancellationToken)
+        {
+            var media = _mapper.ReverseMap(mediaDto);
+            media.ProfileId = userId;
+
+            await _mediaRepository.CreateAsync(media, cancellationToken);
+        }
+
+        public async Task UpdateAsync(MediaDto mediaDto,
+            Guid mediaId,
+            CancellationToken cancellationToken)
+        {
+            var media = _mapper.ReverseMap(mediaDto);
+            media.Id = mediaId;
+
+            await _mediaRepository.UpdateAsync(media, cancellationToken);
+        }
+
+        public async Task DeleteAsync(Guid mediaId, 
+            CancellationToken cancellationToken)
+        {
+            var media = await _mediaRepository.GetByIdAsync(mediaId, cancellationToken);
+            if (media is null)
+            {
+                throw new EntityNotFoundException(typeof(Media), mediaId);
+            }
+
+            await _mediaRepository.DeleteAsync(media, cancellationToken);
+        }
+    }
+}
