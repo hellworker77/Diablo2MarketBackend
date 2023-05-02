86f081609a133fddefa4efd5109a0bb898f4abce
Ilya Safonov <illia220011@gmail.com>
2023-05-03 02:56:09 +0300

    Media mapper has been added.

---
+++ Common/Mappers/MediaMapper.cs
@@ -0,0 +1,18 @@
+﻿using Common.Models;
+using Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Mappers
+{
+    [Mapper]
+    public partial class MediaMapper
+    {
+        public partial MediaDto Map(Media media);
+        public partial Media ReverseMap(MediaDto mediaDto);
+    }
+}
