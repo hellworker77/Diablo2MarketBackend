1ee96a587acf518577077d1391865efde9c03c8f
Ilya Safonov <illia220011@gmail.com>
2023-05-05 16:59:22 +0300

    New ApplicationUserDto properties.

--- Common/Models/ApplicationUserDto.cs
+++ Common/Models/ApplicationUserDto.cs
@@ -5,6 +5,10 @@ namespace Common.Models;
 public class ApplicationUserDto
 {
     public uint Balance { get; set; }
+#pragma warning disable CS8618 
+    public string UserName { get; set; }
+    public string Email { get; set; }
+#pragma warning restore CS8618
     public virtual List<MediaShortDto>? ProfilePictures { get; set; }
     public virtual List<ItemDto>? Items { get; set; }
 }
