aa0030ba0cf5c0a068519db1717fb02776f51790
Ilya Safonov <illia220011@gmail.com>
2023-06-01 16:43:56 +0300

    Deals filters has been added

---
+++ Filters/DealFilterSpecifications.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using Filters.Abstractions;
+using Filters.Deals;
+
+namespace Filters
+{
+    public class DealFilterSpecifications
+    {
+        public static AbstractFilterSpecification<Deal> Last24Hours = new Last24HoursDealsSpecification();
+        public static AbstractFilterSpecification<Deal> InProgress = new InProgressDealsSpecification();
+        public static AbstractFilterSpecification<Deal> Suspended = new SuspendedDealsSpecification();
+        public static AbstractFilterSpecification<Deal> Success = new SuccessDealsSpecification();
+    }
+}
---
+++ Filters/Deals/SuccessDealsSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+using Filters.Abstractions;
+using System.Linq.Expressions;
+
+namespace Filters.Deals
+{
+    internal class SuccessDealsSpecification : AbstractFilterSpecification<Deal>
+    {
+        public override Expression<Func<Deal, bool>> SpecificationExpression => 
+            (deal) => deal.Status == Entities.Enums.DealStatus.Success;
+    }
+}
